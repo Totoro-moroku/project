1dab196328d948f541dd72057e27895b288df6db
Togashi-sys <t.togashi@strategit.jp>
2022-07-01 14:50:59 +0900

    :bug: os

--- index.py
+++ index.py
@@ -8,6 +8,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   return 'Index Page'
+
+
 if __name__ == "__main__":
-  port = int(os.environ.get("PORT",80))
+  port = int(os.environ.get("PORT", 5000))
   app.run(host="0.0.0.0", port=port)
